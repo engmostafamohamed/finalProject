fc76b7d94ebc469ac3c3ff24a34a68e939be36cc
Mostafa <Mostafa@coding.com>
2022-12-11 14:05:50 +0200

    another change in connect frontend with backend

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'Newtest-env.eba-pmsr3fcz.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://localhost:8000/api/v0'
 };
 
 /*

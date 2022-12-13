d01939a3139f9a078478265428322640de3baacf
Mostafa <Mostafa@coding.com>
2022-12-14 01:19:24 +0200

    update on config.yml file and set_env file and deploy file

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -2,4 +2,7 @@ aws s3 cp --recursive --acl public-read ./www s3://udagram-frontend22/
 aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://udagram-frontend22/
 
 
-eb setenv PORT=$PORT USERNAME=$USERNAME
+eb setenv PORT=$PORT POSTGRES_USERNAME=$POSTGRES_USERNAME 
+POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_HOST=$POSTGRES_HOST
+POSTGRES_DB=$POSTGRES_DB AWS_BUCKET=$AWS_BUCKET AWS_REGION=$AWS_REGION AWS_PROFILE=$AWS_PROFILE
+JWT_SECRET=$JWT_SECRET URL=$URL
